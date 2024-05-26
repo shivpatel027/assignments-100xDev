4db0ebfd5f96b1c4958bbbda1c03d98863bbeb8a
shivpatel027 <patelshiv1701@gmail.com>
2024-05-26 13:34:47 -0500

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+  // Remove all non-alphanumeric characters and convert strings to lower case
+  const cleanStr1 = str1.replace(/[^a-z0-9-!]/gi, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^a-z0-9-!]/gi, '').toLowerCase();
 
+  // If lengths of cleaned strings are not equal, they cannot be anagrams
+  if (cleanStr1.length !== cleanStr2.length) {
+      return false;
+  }
+
+  // Sort characters of both strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Compare sorted strings
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
