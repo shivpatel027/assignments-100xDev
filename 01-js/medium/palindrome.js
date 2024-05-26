ad6bd13455e87177a38b6923358fb6105be6ec4f
shivpatel027 <patelshiv1701@gmail.com>
2024-05-26 18:28:45 -0500

    palindrom solve

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,14 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+  // Reverse the cleaned string
+  const reversedStr = cleanStr.split('').reverse().join('');
+  
+  // Check if the cleaned string is the same as the reversed string
+  return cleanStr === reversedStr;
 }
 
 module.exports = isPalindrome;
